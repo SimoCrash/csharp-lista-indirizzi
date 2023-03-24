ce2f7939367bcf36ef8c9e971ac6730b7ac5bdc4
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-24 14:58:05 +0100

    instanziati oggetti nella classe Indirizzo

---
+++ Indirizzo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_lista_indirizzi
+{
+    public class Indirizzo
+    {
+        public Indirizzo(string name, string surname, string street, string city, string province, int ZIP)
+        {
+            Name = name;
+            Surname = surname;
+            Street = street;
+            City = city;
+            Province = province;
+            this.ZIP = ZIP;
+        }
+
+        public string Name { get; init; }
+        public string Surname { get; init; }
+        public string Street { get; init; }
+        public string City { get; init; }
+        public string Province { get; init; }   
+        public int ZIP { get; init; }
+    }
+}
