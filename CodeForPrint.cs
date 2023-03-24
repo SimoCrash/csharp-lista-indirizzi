93c4047f50dc8200b261d8ea7e3f33b4299646d7
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-24 15:50:10 +0100

    push per inizializzazione struttura CodeForPrint

--- CodeForPrint.cs
+++ CodeForPrint.cs
@@ -8,6 +8,22 @@ namespace csharp_lista_indirizzi
 {
     public static class CodeForPrint
     {
-        
+        public static IEnumerable<Indirizzo> Read()
+        {
+            using var input = File.OpenText("addresses.csv");
+            var indirizzi = new List<Indirizzo>();
+            input.ReadLine();
+
+            while (true)
+            {
+                string? line = input.ReadLine();
+                if(line is null) return indirizzi;
+            }
+        }
+        public static void Write(IEnumerable<Indirizzo> Indirizzi)
+        {
+            using var output = File.CreateText("input.csv");
+
+        }
     }
 }
